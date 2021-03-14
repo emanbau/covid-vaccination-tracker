5ab3d910466cffdc757a5defc2cb9a88a875b3cc
Eman Bau <emanbautista410@gmail.com>
2021-03-14 13:24:45 -0400

    Add API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
+import React from 'react';
 import './App.css';
+import './VaccinationTable';
+import {useState, useEffect} from 'react';
+
 
 function App() {
+
+  const api = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.json';
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(api)
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, [])
+
+
   return (
     <div className="App">
 
