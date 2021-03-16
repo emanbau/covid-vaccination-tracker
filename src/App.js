dbcacef079948fe784f264d6a691a44224f3e96e
Eman Bau <emanbautista410@gmail.com>
2021-03-15 21:32:24 -0400

    Implement Axios

--- src/App.js
+++ src/App.js
@@ -1,31 +1,47 @@
 import React from 'react';
 import './App.css';
 import './VaccinationTable';
+import axios from 'axios';
 import {useState, useEffect} from 'react';
+import VaccinationTable from './VaccinationTable';
 
 
 function App() {
 
-  const api = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.json';
-
   const [data, setData] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
-    fetch(api)
-      .then(response => {
-        setData(response.data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
+    axios.get('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.json')
+    .then(response => {
+      setData(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
   }, [])
 
-  
+
+  const handleChange = event => {
+    setSearch(event.target.value);
+  }
+
+  console.log(data);
+
+  const filteredCountry = data.filter(data => 
+    data.country.toLowerCase().includes(search.toLowerCase())
+  )
 
 
   return (
     <div className="App">
-
+      {filteredCountry.map(data => {
+        return(
+          <VaccinationTable
+          
+          />
+        )
+      })}
     </div>
   );
 }
