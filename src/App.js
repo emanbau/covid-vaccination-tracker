1c2d94dd96a0ded8108d1dae065345557fd3f9e7
Eman Bau <emanbautista410@gmail.com>
2021-03-17 19:30:54 -0400

    Table Setup
    Rearranged code

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import VaccinationTable from './VaccinationTable';
 function App() {
 
   const [data, setData] = useState([]);
-  const [search, setSearch] = useState('');
 
   useEffect(() => {
     axios.get('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.json')
@@ -21,29 +20,12 @@ function App() {
   }, [])
 
 
-  // Form Handle - Search for country
-  const handleChange = event => {
-    setSearch(event.target.value);
-  }
-
-  // Filtered Search - Returns country that was searched
-  const filteredCountry = data.filter(data => 
-    data.country.toLowerCase().includes(search.toLowerCase())
-  )
-
-
   return (
     <div className="App">
       <div className="countryTable">
-        {filteredCountry.map(data => {
-          return(
-            <VaccinationTable
-            country={data['country']}
-            data={data['data']}
-            iso={data['iso_code']}
-            />
-          )
-        })}
+      <VaccinationTable
+        data={data}
+        />
       </div>
     </div>
   );
--- src/VaccinationTable.js
+++ src/VaccinationTable.js
@@ -1,23 +1,68 @@
 import React from 'react';
+import { useState } from 'react';
 import './VaccinationTable.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
-function VaccinationTable({country, data, iso}) {
+const useStyles = makeStyles({
+    table: {
+      minWidth: 650,
+    },
+  });
+
+function createData(country, iso, totalVaccinations) {
+    return {country, iso, totalVaccinations};
+}
+
+let rows = []
+
+function VaccinationTable({ data }) {
+
+    const classes = useStyles();
+
+    const [search, setSearch] = useState('');
+
+    // Form Handle - Search for country
+    const handleChange = event => {
+        setSearch(event.target.value);
+    }
+
+    // Filtered Search - Returns country that was searched
+    const filteredCountry = data.filter(data => 
+        data.country.toLowerCase().includes(search.toLowerCase())
+    )
 
     // Filter through data to find most recent numbers
-    let total;
+    filteredCountry.map(data => {
+        let total;
+        let countryName = data["country"];
+        let iso = data["iso_code"];
 
-    for (let i = data.length - 1; i >= 0; i--) {
-        if ("total_vaccinations" in data[i]) {
-            total = data[i]["total_vaccinations"];
-            break;
-        } else {
-            total = 'DNE'
+        for (let i = data["data"].length - 1; i >= 0; i--) {
+            if ("total_vaccinations" in data["data"][i]) {
+                total = data["data"][i]["total_vaccinations"];
+                break;
+            } else {
+                total = 'DNE';
+            }
         }
-    }
-    
+
+        let dataPush = createData(countryName, iso, total);
+        rows.push(dataPush);
+        
+      })
+
+      console.log(rows);
+
     return (
-        <div className='tableContainer'>
-            <h1>{country}: {total} Vaccinations Total</h1>
+        <div className='table'>
+
         </div>
     )
 }
