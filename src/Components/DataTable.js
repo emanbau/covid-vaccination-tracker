24eb8a649f958d22cc5ffda0eedf025b14d119f2
Eman Bau <emanbautista410@gmail.com>
2021-03-23 14:01:06 -0400

    Restructuring files

---
+++ src/Components/DataTable.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import './ChoroplethMap';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+    tableContainerDiv: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: '100vw',
+    },
+
+    searchbar: {
+        display: 'block',
+        width: '100%',
+        marginTop: '2em',
+        marginBottom: '2em',
+        padding: '10px',
+        borderRadius: '16px 16px 16px 16px',
+        fontSize: '15px',
+        border: '.5px solid black',
+        textAlign: 'center',
+        outline: 'none',
+    },
+
+    tableContainer: {
+        minWidth: 300,
+        maxWidth: 500,
+    },
+
+    table: {
+        minWidth: 300,
+    },
+
+    headCell: {
+        paddingLeft: '3em',
+        paddingRight: '3em',
+        paddingTop: '1.5em',
+        paddingBottom: '1.5em',
+        backgroundColor: '#fafafa',
+    },
+    bodyCell: {
+        paddingLeft: '3em',
+        paddingRight: '3em',
+    },
+  });
+
+function DataTable({rows, handleChange}) {
+
+    const classes = useStyles();
+
+    return (
+        <div className={classes.tableContainerDiv}>
+                <form>
+                    <input className={classes.searchbar} type='text' placeholder='Search Country' onChange={handleChange}/>
+                </form>
+                <Paper elevation={9}>
+                    <TableContainer className={classes.tableContainer}>
+                        <Table className={classes.table}>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell className={classes.headCell}>Country</TableCell>
+                                    <TableCell className={classes.headCell} align='left'>Total Vaccinations</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                            {rows.map( element => {
+                                return(
+                                    <TableRow>
+                                        <TableCell className={classes.bodyCell}>{element.country}</TableCell>
+                                        <TableCell className={classes.bodyCell} align='left'>{element.totalVaccinations}</TableCell>
+                                    </TableRow>
+                                )
+                            })}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Paper>
+            </div>
+    )
+}
+
+export default DataTable
--- src/Components/VaccinationTable.js
+++ src/Components/VaccinationTable.js
@@ -1,59 +1,8 @@
 import React from 'react';
 import { useState } from 'react';
-import './VaccinationTable.css';
-import './ChoroplethMap';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import DataTable from './DataTable';
 import ChoroplethMap from './ChoroplethMap';
 
-const useStyles = makeStyles({
-    tableContainerDiv: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        width: '100vw',
-    },
-
-    searchbar: {
-        display: 'block',
-        width: '100%',
-        marginTop: '2em',
-        marginBottom: '2em',
-        padding: '10px',
-        borderRadius: '16px 16px 16px 16px',
-        fontSize: '15px',
-        border: '.5px solid black',
-        textAlign: 'center',
-        outline: 'none',
-    },
-
-    tableContainer: {
-        minWidth: 300,
-        maxWidth: 500,
-    },
-
-    table: {
-        minWidth: 300,
-    },
-
-    headCell: {
-        paddingLeft: '3em',
-        paddingRight: '3em',
-        paddingTop: '1.5em',
-        paddingBottom: '1.5em',
-        backgroundColor: '#fafafa',
-    },
-    bodyCell: {
-        paddingLeft: '3em',
-        paddingRight: '3em',
-    },
-  });
 
 function createData(country, totalVaccinations) {
     return {country, totalVaccinations};
@@ -62,8 +11,6 @@ function createData(country, totalVaccinations) {
 let rows = []
 
 function VaccinationTable({ data }) {
-
-    const classes = useStyles();
 
     const [search, setSearch] = useState('');
 
@@ -102,33 +49,7 @@ function VaccinationTable({ data }) {
     return (
         <div>
             <ChoroplethMap filteredData={rows}/>
-            <div className={classes.tableContainerDiv}>
-                <form>
-                    <input className={classes.searchbar} type='text' placeholder='Search Country' onChange={handleChange}/>
-                </form>
-                <Paper elevation={9}>
-                    <TableContainer className={classes.tableContainer}>
-                        <Table className={classes.table}>
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell className={classes.headCell}>Country</TableCell>
-                                    <TableCell className={classes.headCell} align='left'>Total Vaccinations</TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                            {rows.map( element => {
-                                return(
-                                    <TableRow>
-                                        <TableCell className={classes.bodyCell}>{element.country}</TableCell>
-                                        <TableCell className={classes.bodyCell} align='left'>{element.totalVaccinations}</TableCell>
-                                    </TableRow>
-                                )
-                            })}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                </Paper>
-            </div>
+            <DataTable rows={rows} handleChange={handleChange}/>
         </div>
     )
 }
